18a75f6743daf3560a22db3dd33418dbe0e68989
Numonbek <numonbek1996@gmail.com>
2022-02-07 22:39:29 +0300

    deploy fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-const API_URL = `http://api.football-data.org/v2/`;
+const API_URL = `https://api.football-data.org/v2/`;
 
 const HEADER = {
   headers: {
